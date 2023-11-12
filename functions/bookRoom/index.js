65a736cf099bdec7b2482030f8b8cd52bdeaa8cd
Antonio Saracino <antonio.saracino83@gmail.com>
2023-11-12 17:34:46 +0100

    init

---
+++ functions/bookRoom/index.js
@@ -0,0 +1,11 @@
+import { sendResponse } from "../../responses";
+
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+
+    // TODO
+
+
+}
