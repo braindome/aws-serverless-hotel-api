26a3e2f007bb601da20cc9d5b7b596fbc824c2bc
Antonio Saracino <antonio.saracino83@gmail.com>
2023-11-14 11:09:49 +0100

    basic room booking function

--- functions/bookRoom/index.js
+++ functions/bookRoom/index.js
@@ -2,12 +2,36 @@ import { sendResponse } from "../../responses";
 
 const AWS = require("aws-sdk");
 const db = new AWS.DynamoDB.DocumentClient();
+const { v1: uuidv1 } = require("uuid");
+
+const uuidTest = uuidv1();
+console.log(uuidTest);
 
 exports.handler = async (event, context) => {
+  const bookingDetails = JSON.parse(event.body);
 
-    // TODO
-    // TODO
+  bookingDetails.id = uuidv1();
+  bookingDetails.numberOfGuests = bookingDetails.numberOfGuests;
+  bookingDetails.checkInDate = bookingDetails.checkInDate;
+  bookingDetails.checkOutDate = bookingDetails.checkOutDate;
+  bookingDetails.rooms = bookingDetails.rooms;
+  bookingDetails.referencePerson = bookingDetails.referencePerson;
 
 
-}
-//Johan
+  try {
+    await db
+      .put({
+        TableName: "hotel-db",
+        Item: bookingDetails,
+      })
+      .promise();
+    return sendResponse(200, {
+      success: true,
+      bookingConfirmation: bookingDetails
+    });
+  } catch (error) {
+    return sendResponse(500, {
+      success: false,
+    });
+  }
+};
